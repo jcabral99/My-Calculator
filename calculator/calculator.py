afe3afcc59807b94b7eb0b7fa240afc701c6db56
jcabral99 <jc2447@njit.edu>
2021-11-11 21:32:38 -0500

    new commits

---
+++ calculator/calculator.py
@@ -0,0 +1,43 @@
+"""This is the increment function"""
+# first import the addition namespace
+from calc.addition import Addition
+from calc.subtraction import Subtraction
+from calc.multiplication import Multiplication
+
+class Calculator:
+    """This is the Calculator class"""
+    history = []
+    @staticmethod
+    def history_count():
+        return len(Calculator.history)
+    @staticmethod
+    def add_calculation_to_history(calculation):
+        Calculator.history.append(calculation)
+        return True
+    @staticmethod
+    def get_result_of_last_calculation_added_to_history():
+        # -1 gets the last item added to the list automatically and you can expect it to have the get result method
+        return Calculator.history[-1].getresult()
+    @staticmethod
+    def add_number(value_a, value_b):
+        """adds number to result"""
+        #create an addition object using the factory we created on the calculation class
+        addition = Addition.create(value_a,value_b)
+        # addition = addition(value_a, value_b) <-this is not good but will work. It will be repeated too much
+        Calculator.add_calculation_to_history(addition)
+        return Calculator.get_result_of_last_calculation_added_to_history()
+    @staticmethod
+    def subtract_number(value_a, value_b):
+        """"subtract number from result"""
+        # create an subtract object using the factory we created on the calculation class
+        subtraction = Subtraction.create(value_a, value_b)
+         # addition = addition(value_a, value_b) <-this is not good but will work. It will be repeated too much
+        Calculator.add_calculation_to_history(subtraction)
+        return Calculator.get_result_of_last_calculation_added_to_history()
+
+    @staticmethod
+    def multiply_numbers(value_a, value_b):
+        """multiply two numbers and store the result"""
+        multiplication = Multiplication.create(value_a,value_b)
+        Calculator.add_calculation_to_history(multiplication)
+        return Calculator.get_result_of_last_calculation_added_to_history()
---
+++ tests/calculator_test.py
@@ -0,0 +1,22 @@
+"""Testing the Calculator"""
+import pprint
+
+from calculator.calculator import Calculator
+
+def test_calculator_add():
+    """testing the add function of the calculator"""
+    assert Calculator.add_number(1,2) == 3
+    assert Calculator.add_number(2, 2) == 4
+    assert Calculator.add_number(3, 2) == 5
+    assert Calculator.add_number(4, 2) == 6
+    assert Calculator.history_count() == 4
+    assert Calculator.get_result_of_last_calculation_added_to_history() == 6
+    pprint.pprint(Calculator.history)
+
+def test_calculator_subtract():
+    """Testing the subtract method of the calculator"""
+    assert Calculator.subtract_number(1,2) == -1
+
+def test_calculator_multiply():
+    """test multiplication of two numbers"""
+    assert Calculator.multiply_numbers(1,2) == 2
