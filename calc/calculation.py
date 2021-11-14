d8e5c01c56b9036242051cd16b92283323522048
jcabral99 <jc2447@njit.edu>
2021-11-13 20:27:04 -0500

    changes

--- calc/calculation.py
+++ calc/calculation.py
@@ -1,9 +1,7 @@
 """This is our calculation base class / Abstract Class"""
 class Calculation:
 
-    #constructor and it is the first function called when an object of the class is instantiated(created)
     def __init__(self,value_a, value_b):
-        #self references the instantiated object of the class
         self.value_a = value_a
         self.value_b = value_b
     # class factory method
--- calculator/calculator.py
+++ calculator/calculator.py
@@ -23,22 +23,17 @@ class Calculator:
         return True
     @staticmethod
     def get_result_of_last_calculation_added_to_history():
-        # -1 gets the last item added to the list automatically and you can expect it to have the get result method
         return Calculator.history[-1].getresult()
     @staticmethod
     def add_number(value_a, value_b):
         """adds number to result"""
-        #create an addition object using the factory we created on the calculation class
         addition = Addition.create(value_a,value_b)
-        # addition = addition(value_a, value_b) <-this is not good but will work. It will be repeated too much
         Calculator.add_calculation_to_history(addition)
         return Calculator.get_result_of_last_calculation_added_to_history()
     @staticmethod
     def subtract_number(value_a, value_b):
         """"subtract number from result"""
-        # create an subtract object using the factory we created on the calculation class
         subtraction = Subtraction.create(value_a, value_b)
-         # addition = addition(value_a, value_b) <-this is not good but will work. It will be repeated too much
         Calculator.add_calculation_to_history(subtraction)
         return Calculator.get_result_of_last_calculation_added_to_history()
 
