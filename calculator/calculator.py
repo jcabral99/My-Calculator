cdd829b0cc1ee9fb5ed98c8bb9dc9840c5259980
jcabral99 <jc2447@njit.edu>
2021-11-13 20:21:58 -0500

    changed

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,5 +1,4 @@
 """This is the increment function"""
-# first import the addition namespace
 from calc.addition import Addition
 from calc.subtraction import Subtraction
 from calc.multiplication import Multiplication
