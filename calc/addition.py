f60897065082747667476934f48342959682d200
jcabral99 <jc2447@njit.edu>
2021-11-18 17:06:11 -0500

    adding tests

--- calc/addition.py
+++ calc/addition.py
@@ -1,9 +1,12 @@
-"""this is the addition calculation that is being inherits the value A and value B from the calculation class"""
-
-from calc.calculation import Calculation
+"""Addition Class"""
+from calc.calculations.calculation import Calculation
 
 class Addition(Calculation):
-    """The addition class has one method to get the result of the calculation A and B come from
-    the calulation parent"""
-    def getresult(self):
-        return self.value_a + self.value_b
+    """ calculation addition class"""
+    def get_result(self):
+        """get the addition results"""
+        sum_of_values = 0.0
+        for value in self.values:
+            sum_of_values = value + sum_of_values
+        return sum_of_values
+
