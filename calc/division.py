e686cebf4c654075f7d0c76b82e7bc1d199eaba7
jcabral99 <jc2447@njit.edu>
2021-11-18 17:39:22 -0500

    addings classes and tests

--- calc/division.py
+++
@@ -1,7 +0,0 @@
-from calc.calculation import Calculation
-
-class Division(Calculation):
-    """The addition class has one method to get the result of the calculation A and B come from
-    the calulation parent"""
-    def getresult(self):
-        return self.value_a / self.value_b
