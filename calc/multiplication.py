0f7c08bfe27cdee35f0e5a8b8c91c9fa78ab6fe9
jcabral99 <jc2447@njit.edu>
2021-12-04 16:42:59 -0500

    updating homework

---
+++ calc/multiplication.py
@@ -0,0 +1,9 @@
+"""this is the addition calculation that is being inherits the value A and value B from the calculation class"""
+
+from calc.calculation import Calculation
+
+class Multiplication(Calculation):
+    """The addition class has one method to get the result of the calculation A and B come from
+    the calulation parent"""
+    def getresult(self):
+        return self.value_a * self.value_b
---
+++ main.py
@@ -0,0 +1,101 @@
+import glob
+import pathlib
+import os
+import pandas as pd
+import time
+
+from calculator.calculator import Calculator
+
+def process(files):
+    for file in files:
+        basename = os.path.basename(file)
+        # print(basename)
+        if basename == "addition.csv":
+            print("add")
+            df = pd.read_csv(file)
+            results_arr = []
+            for index, row in df.iterrows():
+                temp = Calculator.add_number(row['Value_1'], row['Value_2'])
+                with open("results/results.txt", "a") as fp:
+                    fp.write(f"Time: {time.time()}, File: {file}, Record Number: {index}, Operation: add, Result: {temp}\n")
+                    fp.close()
+                results_arr.append(temp)
+            df["Result"] = results_arr
+            with open(f"output_csvs/{basename}", "w") as fp:
+                df.to_csv(fp)
+        if basename == "subtraction.csv":
+            print("sub")
+            df = pd.read_csv(file)
+            results_arr = []
+            for index, row in df.iterrows():
+                temp = Calculator.subtract_number(row['Value_1'], row['Value_2'])
+                with open("results/results.txt", "a") as fp:
+                    fp.write(
+                        f"Time: {time.time()}, File: {file}, Record Number: {index}, Operation: subtract, Result: {temp}\n")
+                    fp.close()
+                results_arr.append(temp)
+            df["Result"] = results_arr
+            with open(f"output_csvs/{basename}", "w") as fp:
+                df.to_csv(fp)
+        if basename == "multiplication.csv":
+            print("multiply")
+            df = pd.read_csv(file)
+            results_arr = []
+            for index, row in df.iterrows():
+                temp = Calculator.multiply_numbers(row['Value_1'], row['Value_2'])
+                with open("results/results.txt", "a") as fp:
+                    fp.write(
+                        f"Time: {time.time()}, File: {file}, Record Number: {index}, Operation: multiply, Result: {temp}\n")
+                    fp.close()
+                results_arr.append(temp)
+            df["Result"] = results_arr
+            with open(f"output_csvs/{basename}", "w") as fp:
+                df.to_csv(fp)
+        if basename == "division.csv":
+            print("division")
+            df = pd.read_csv(file)
+            results_arr = []
+            # Loop Through records
+            for index, row in df.iterrows():
+                # Catch divide by 0 error
+                if row['Value_2'] == 0:
+                    with open("results/exceptions.txt", "a") as fp:
+                        fp.write(f"File: {file}, Record Number: {index}\n")
+                        fp.close()
+                    results_arr.append("NaN")
+                else:
+                    # Operation
+                    temp = Calculator.divide_numbers(row['Value_1'], row['Value_2'])
+                    # Write to log
+                    with open("results/results.txt", "a") as fp:
+                        fp.write(f"Time: {time.time()}, File: {file}, Record Number: {index}, Operation: divide, Result: {temp}\n")
+                        fp.close()
+                    results_arr.append(temp)
+
+            # Make output CSVs
+            df["Result"] = results_arr
+            with open(f"output_csvs/{basename}", "w") as fp:
+                df.to_csv(fp)
+
+    return 0
+
+def main():
+    path = pathlib.Path(__file__).parent / "input_csvs"
+    # print ("path: " + str(path))
+    files = glob.glob(str(path) + "/*")
+    # print(files)
+    files_len = len(files)
+    while True:
+        if files_len is not 0:
+            print("Processing...")
+            done = process(files)
+            files_len = done
+        else:
+            print("Running |", end="\r")
+            print("Running /", end="\r")
+            print("Running -", end="\r")
+            print("Running \\", end="\r")
+        
+    return True
+
+main()
