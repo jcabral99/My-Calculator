7a08e93b7e45bb0a168e6476e1086a33a8c69a64
jcabral99 <jc2447@njit.edu>
2021-12-14 20:13:23 -0500

    flash messages

--- app/controllers/calculator_controller.py
+++ app/controllers/calculator_controller.py
@@ -1,5 +1,5 @@
 from app.controllers.controller import ControllerBase
-from app.calculator import Calculator
+from app.calulator import Calculator
 from flask import render_template, request, flash, redirect, url_for
 
 
@@ -21,7 +21,7 @@ class CalculatorController(ControllerBase):
             getattr(Calculator, operation)(my_tuple)
             result = str(Calculator.get_last_result_value())
             return render_template('result.html', value1=value1, value2=value2, operation=operation, result=result)
-        return render_template('calculator2.html', error=error)
+        return render_template('calculator.html', error=error)
     @staticmethod
     def get():
-        return render_template('calculator2.html')
+        return render_template('calculator.html')
